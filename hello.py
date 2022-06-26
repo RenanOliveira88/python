2d4fe0e9295c97b6cf6973c5bc0d3131036976db
renan <c.renan.o@gmail.com>
2022-06-26 08:35:45 -0300

    Hello world com flask - routing

--- hello.py
+++ hello.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
 @app.route('/')
+def hello_name():
+    return '<p>index page!</p>'
+
+@app.route('/hello')
 def hello_world():
     return '<p>hello world</p>'
+
