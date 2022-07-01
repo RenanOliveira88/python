a2797f4fdf097bcef421500715a55a7f00bd2327
renan <c.renan.o@gmail.com>
2022-07-01 20:11:23 -0300

    Flask tutorial - Blueprints and Views auth.py

--- flask_tutorial/flaskr/__init__.py
+++ flask_tutorial/flaskr/__init__.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Flask
 
+
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
@@ -16,19 +17,21 @@ def create_app(test_config=None):
         # Load the test config if passed in
         app.config.from_mapping(test_config)
 
-    #ensure the instance folder exists
+    # Ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
 
-    #a simple page that says Hello
+    # a simple page that says Hello
     @app.route('/hello')
     def hello():
         return 'Hello, world!'
 
     from . import db
     db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
     return app
-
-
